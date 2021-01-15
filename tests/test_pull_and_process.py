8d6608a4124ab55dbfe10c08730d9efe9ac19c72
DillonNMorse <DillonNMorse@gmail.com>
2021-01-15 16:47:48 -0700

    Clean up a bit

--- tests/test_pull_and_process.py
+++ tests/test_pull_and_process.py
@@ -14,12 +14,18 @@ import pull_and_process as pap
 
 class TestPap(unittest.TestCase):
     
-    def test_auth(self):
-        test_auth = pap.auth('./test_auth.txt')
+    def test_get_auth(self):
+        test_auth = pap.get_auth('./test_auth.txt')
         self.assertEqual(test_auth.client_id, '555ABCD111')
         self.assertEqual(test_auth.client_secret, '1j5tr4katesttest')
         self.assertEqual(test_auth.user_agent, 'test_agent_please')
 
+    def test_subreddit_list(self):
+        test_subreddit_list = (pap
+                               .get_subreddit_list('./test_subreddit_list.txt')
+                               )
+        validation_list = ['subreddit1', 'subreddit2', 'subreddit3']
+        self.assertEqual(test_subreddit_list, validation_list)
 
 
 if __name__ == '__main__':
